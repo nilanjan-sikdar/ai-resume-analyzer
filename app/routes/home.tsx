c4f40764380048441c354037180dee0baea9d57f
nilanjan-sikdar <nilanjansikdar66@gmail.com>
2025-12-28 15:24:39 +0530

    add authentication using puter

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,10 @@
 import type { Route } from "./+types/home";
 import Navbar from "../components/Navbar";
 import { resumes } from "../../constants";
-import ResumeCard from "../../constants/ResumeCard";
+import ResumeCard from "../components/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
+import { useLocation, useNavigate } from "react-router";
+import { useEffect } from "react";
 export function meta({ }: Route.MetaArgs) {
   return [
     { title: "ThugResume" },
@@ -10,21 +13,28 @@ export function meta({ }: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated])
+
   return <main className="bg-[url('/images/bg-main.jpg')] bg-cover">
     <Navbar />
     <section className="main-section">
-      <div className="page-heading">
+      <div className="page-heading py-16">
         <h1>Crack Your Resume Before Interviewer Crack it!</h1>
         <h2>Upload your resume and let our AI analyze it for you</h2>
       </div>
-    </section>
 
-    {resumes.length > 0 && (
-      <div className="resumes-section">
-        {resumes.map((resume) => (
-          <ResumeCard key={resume.id} resume={resume} />
-        ))}
-      </div>
-    )}
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </section>
   </main>
 }
